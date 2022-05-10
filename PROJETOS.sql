7eb913cd71b7b35e66c7a69de751bec8df09b2db
Pedro Nunes Moura <61888421+Pnmoura@users.noreply.github.com>
2022-05-09 23:30:21 -0300

    Add files via upload

---
+++ PROJETOS.sql
@@ -0,0 +1,8 @@
+USE UpperComputer;
+go
+
+INSERT INTO tNoite.PROJETOS (idprojeto,idequipe,idatividade,nomegerente,nomeprojeto,salarioger,datainicio,dataprevista,datafim,andamento,hrprevistas,hrutilizadas)
+VALUES (10,12,15,'Rogerio','Porto',1505.06,'26/05/2021','13/07/2021','26/08/2021','Em teste','14:56:27','13:20:30')
+
+select * from tNoite.PROJETOS;
+
