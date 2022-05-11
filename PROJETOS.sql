737d0d4c6bcbb63fa1afc6a07172ce5d95623a24
Pedro Nunes Moura <61888421+Pnmoura@users.noreply.github.com>
2022-05-11 14:03:10 -0300

    Update PROJETOS.sql

--- PROJETOS.sql
+++ PROJETOS.sql
@@ -5,4 +5,3 @@ INSERT INTO tNoite.PROJETOS (idprojeto,idequipe,idatividade,nomegerente,nomeproj
 VALUES (10,12,15,'Rogerio','Porto',1505.06,'26/05/2021','13/07/2021','26/08/2021','Em teste','14:56:27','13:20:30')
 
 select * from tNoite.PROJETOS;
-
